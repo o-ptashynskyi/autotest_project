08b2c3b902b9ca37039e014fb7ddb42eb62735a0
alex <mailest@mail.com>
2022-08-28 19:37:19 +0300

    standart tests were removed to base_page class

--- pages/locators.py
+++ pages/locators.py
@@ -14,3 +14,7 @@ class ProductPageLocators():
     CONFIRM_MESSAGE = (By.CSS_SELECTOR, '#messages > div:nth-child(1) > div > strong')
     BASKET_TOTAL = (By.CSS_SELECTOR, '#messages > div.alert.alert-safe.alert-noicon.alert-info.fade.in > div > p:nth-child(1) > strong')
     SUCCESS_MESSAGE = (By.CSS_SELECTOR, '#messages > div:nth-child(1)')
+
+class BasePageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
